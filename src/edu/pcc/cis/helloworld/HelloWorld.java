df8e9b51af2e7a351a10a6e5b004ba7924eaa9aa
Kelly Nair <kelly.nair@pcc.edu>
2019-10-21 23:54:33 -0700

    added date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -3,15 +3,20 @@ package edu.pcc.cis.helloworld;
 //author: Kelly Nair
 // date: 2019.21.10
 
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Have a great day!");
+
         char c = 'A';
 
         do {
             System.out.println(c);
             c++;
         } while (c <= 'Z');
+
+        System.out.println(new Date());
     }
 
 
